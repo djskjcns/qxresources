e212aa7fd204b7a16a51ec8246c2eadf127187b1
djskjcns <79205062+djskjcns@users.noreply.github.com>
2024-10-07 16:36:24 +0800

    Update resource-parser.js - Removed the 'ip-asn' format handling in the parser.

--- resource-parser.js
+++ resource-parser.js
@@ -4,19 +4,23 @@
  * @supported Quantumult X (v1.0.8-build253)
  */
 
+// $resource, $notify(title, subtitle, message)
+// HTTP reqeust and persistent storage related APIs are not supported in resource parser.
+
+// $resource.link contains the original URL of the resource or the path if the resource is local.
+// $resource.content contains the response(UTF8) from the URL .
+
+// $done({error : "error description"});
+// $done({content : "the modified content"});
+
 function parseResource(content) {
     // Split content by lines
     const lines = content.split('\n');
     const result = lines.map(line => {
         line = line.trim();
-        if (line.startsWith('//')) {
+        if (line.startsWith('#')) {
             // Ignore comment lines
             return null;
-        } else if (line.startsWith('IP-ASN')) {
-            // Convert IP-ASN to ip-asn format
-            const parts = line.split(',');
-            const asnPart = parts[1].split(' ')[0].trim(); // Extract the ASN number only, ignoring comments
-            return `ip-asn, ${asnPart}, proxy`;
         } else if (line.includes('/')) {
             // Identify IPv4 and IPv6 CIDR
             if (line.includes(':')) {
